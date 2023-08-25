d7f8322153870dea39cd30820c0f1116be14f64c
Pablo Bassil <pabcubus@gmail.com>
2023-08-24 21:59:08 -0500

    Este es el primer commit

---
+++ nobody1.js
@@ -0,0 +1,24 @@
+function tiempoEnMinutos(horas, mins) {
+    const horasEnMins = horas * 60;
+
+    return horasEnMins + mins;
+}
+
+function cantidadRecolectadaSimple(costo, participantes) {
+    const total = costo * participantes;
+
+    return total;
+}
+
+function cantidadRecolectada(
+    costoEstudiante,
+    costoAdulto,
+    cantidadEstudiantes,
+    cantidadAdultos
+) {
+    return cantidadRecolectadaSimple(costoEstudiante, cantidadEstudiantes) + cantidadRecolectadaSimple(costoAdulto, cantidadAdultos);
+}
+
+const resultado = cantidadRecolectada(2, 10, 5, 2);
+
+console.log(resultado);
